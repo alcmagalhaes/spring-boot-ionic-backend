6481dc7b7a63cf2d928976598b7a37415aac7f41
André Magalhães <alcmagalhaes@gmail.com>
2019-01-14 16:34:50 -0200

    Insercao de novo Pedido com POST

--- src/main/java/com/teste/cursomc/services/PedidoService.java
+++ src/main/java/com/teste/cursomc/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.teste.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.teste.cursomc.domain.ItemPedido;
+import com.teste.cursomc.domain.PagamentoComBoleto;
 import com.teste.cursomc.domain.Pedido;
+import com.teste.cursomc.domain.enums.EstadoPagamento;
+import com.teste.cursomc.repositories.ItemPedidoRepository;
+import com.teste.cursomc.repositories.PagamentoRepository;
 import com.teste.cursomc.repositories.PedidoRepository;
 import com.teste.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -13,12 +20,49 @@ import com.teste.cursomc.services.exceptions.ObjectNotFoundException;
 public class PedidoService {
 	
 	@Autowired
-	private PedidoRepository repository;
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public Pedido find(Integer id) {
-		Optional<Pedido> obj = repository.findById(id);
+		Optional<Pedido> obj = pedidoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preecherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = pedidoRepository.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+				
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+		
 	}
 
 }
